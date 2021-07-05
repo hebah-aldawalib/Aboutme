efc635316d57ffd053fdf4662582cf996ad55918
hebah al daealieb <hebah20082gmail.com>
2021-07-05 16:03:49 +0300

    lab2

---
+++ app.js
@@ -0,0 +1,52 @@
+let favoriteSport=prompt('is football your favorite sport?')
+favoriteSport=favoriteSport.toLowerCase();
+
+  if (favoriteSport === "yes")
+  {
+  console.log('befor','after');
+
+  
+      alert('good');
+  }
+      
+   else 
+   {
+       alert('unfortunately')
+       
+   }
+  
+  
+   let swimming=prompt('do you like swimming?')
+      if (swimming=== "yes")
+      {
+          alert('wawooo')
+
+      }
+  else 
+  {
+      alert('unfortunately')
+  }
+  
+  
+  let basket=prompt('do you like backetball?')
+  if (basket === "yes")
+  { 
+      alert('nice')
+  }
+else{
+    alert('goodluck')
+}
+
+
+  
+      
+
+ 
+
+
+
+
+  
+
+
+      
