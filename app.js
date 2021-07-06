b94babc19e062e51ca5b5fb7ba1029faa7f13f3b
hebah al daealieb <hebah20082gmail.com>
2021-07-06 15:20:59 +0300

    lab3

--- app.js
+++ app.js
@@ -1,52 +1,71 @@
-let favoriteSport=prompt('is football your favorite sport?')
-favoriteSport=favoriteSport.toLowerCase();
+let favoriteSport = prompt('is football my favorite sport?')
+favoriteSport = favoriteSport.toLowerCase();
+let score=0;
+if (favoriteSport === "yes") {
+    console.log('befor', 'after');
 
-  if (favoriteSport === "yes")
-  {
-  console.log('befor','after');
 
-  
-      alert('good');
-  }
-      
-   else 
-   {
-       alert('unfortunately')
-       
-   }
-  
-  
-   let swimming=prompt('do you like swimming?')
-      if (swimming=== "yes")
-      {
-          alert('wawooo')
+    alert('good');
+    score++;
+}
 
-      }
-  else 
-  {
-      alert('unfortunately')
-  }
-  
-  
-  let basket=prompt('do you like backetball?')
-  if (basket === "yes")
-  { 
-      alert('nice')
-  }
-else{
+else {
+    alert('unfortunately')
+
+}
+
+
+let swimming = prompt('i like swimming?')
+if (swimming === "yes") {
+    alert('wawooo')
+
+}
+else {
+    alert('unfortunately')
+}
+
+
+let basket = prompt('i like backetball?')
+if (basket === "yes") {
+    alert('nice')
+}
+else {
     alert('goodluck')
 }
 
 
-  
-      
+let guess = prompt('guess number between 1-6')
+while (guess != "6") {
+    alert("not correct")
+    guess = prompt('guess number between 1-6')
+}
+alert("corret")
+// if (guess ==) {
+//     console.log('not correct');
+// }
+// else if (guess == '6') {
+//     console.log('correct answer')
+// }
+// else {
+//     console.log('pls answer')
+// }
 
- 
+
+let color = ['blue', 'pink', 'black', 'yallow'];
+console.log(color.length);
+let fav=prompt('what is my bed color?');
+
+for (let i = 0; i < color.length; i ++) {
+    console.log(color[i]);
+}
 
 
 
 
-  
 
 
-      
+
+
+
+
+
